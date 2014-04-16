840304643dd6ad3e633984ff2e2182bf2419f32e
Martin <ogradymartin7@gmail.com>
2014-04-16 18:58:15 +0100

    Added hover functionality

--- js/one.js
+++ js/one.js
@@ -7,3 +7,12 @@ $('.dropdown').on('show.bs.dropdown', function(e){
 $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
+
+$('.hover-overlay').hover(
+      function () {
+       $(this).stop().animate({'opacity':'1'},500);
+      }, 
+      function () {
+        $(this).stop().animate({'opacity':'0.0'},500);
+      }
+);
