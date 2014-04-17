1fe5741840ea993b317049088af4a7282d63d99e
Martin <ogradymartin7@gmail.com>
2014-04-17 23:07:22 +0100

    Updated the js for hover functionality

--- js/bootstrap.custom.js
+++ js/bootstrap.custom.js
@@ -8,7 +8,17 @@ $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
 
-$('.hover-overlay').hover(
+$('.image-hover-overlay').hover(
+      function () {
+       $(this).stop().animate({'opacity':'1'},500);
+      }, 
+      function () {
+        $(this).stop().animate({'opacity':'0.0'},500);
+      }
+);
+
+
+$('.text-hover-overlay').hover(
       function () {
        $(this).stop().animate({'opacity':'1'},500);
       }, 
